7feecdf3b22f2e61c476948b0c0ef54439d3a0c1
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-20 12:06:32 -0400

    added fetch from github

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
 
 class GitHubUser extends Component {
+    state = {
+        user: {
+            avatar_url: '',
+            login: '',
+            followers: '',
+            following: '',
+            location: '',
+            html_url: '',
+        }
+    }
+
+    constructor(props) {
+        super(props)
+        this.fetchUserData()
+    }
+
+    fetchUserData = () => {
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => console.log(user))
+    }
+
     render() {
-        return <p>Hello</p>
+        return <h1>Hello</h1>
     }
 }
 
