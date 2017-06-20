0a160963bfd63631b322bc71b033b7fcf31b6942
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-20 11:44:51 -0400

    added functionality to input

--- src/GitHub.js
+++ src/GitHub.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import './GitHub.css'
 
 class GitHub extends Component {
+    state = {
+        username: ''
+    }
+
+    handleChange = (ev) => {
+        ev.preventDefault()
+
+        const username = ev.currentTarget.value
+        this.setState({ username })
+    }
+
     render() {
         return (
             <div className="github">
                 <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo" className="github-logo" />
                 <form>
                     <div>
-                        <input type="text" />
+                        <input
+                            type="text"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div>
                         <button type="submit">Look Up GitHub User</button>
