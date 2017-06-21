5a96abe986216ecba0169268d5fff6a01d8fd92e
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-20 22:34:16 -0400

    removed pointless links in commants and readme.md

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # api-party
-
-* http://pokeapi.co/api/v2/pokemon-species/
-* https://pokeapi.co/docsv2/#pokemon-section
-* These url's contains the data for all pokemon
-
-* Please Note: The api's do nto function properly when accessed through github-pages
--- src/Monster.js
+++ src/Monster.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import './Monster.css'
 
-// http://pokeapi.co/api/v2/pokemon-species/
-// https://pokeapi.co/docsv2/#pokemon-section
-// These url's contains the data for all pokemon
-
 class Monster extends Component {
     state = {
         mon: {
